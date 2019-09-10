cfa22e203e851d48e9966ad257fc8afd986752ab
Jason Jones <jones0401@gmail.com>
2019-09-10 16:12:44 -0500

    More styles added.  Main layout completed

--- gallery/tailwind.config.js
+++ gallery/tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
+  important: true,
   theme: {
-    extend: {}
+    fontFamily: {
+      display: ['Gayathri', 'sans-serif'],
+      body: ['Poppins', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        light: "#d5f4e6",
+        main: "#80ced6",
+        pale: "#fefbd8",
+        dark: "#618685",
+        accent: "#c94c4c"
+      }
+     }
   },
   variants: {},
   plugins: []
