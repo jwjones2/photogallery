a6f1d0dc60ba41a497ae946655d1e6dc5ad280c1
Jason Jones <jones0401@gmail.com>
2019-09-10 22:21:29 -0500

    Groups displaying

--- gallery/src/app/app-routing.module.ts
+++ gallery/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { EventComponent } from './components/event/event.component';
 import { GroupComponent } from './components/group/group.component';
 import { GroupsComponent } from './components/groups/groups.component';
+import { GroupDetailComponent } from './components/group-detail/group-detail.component';
 
 
 const routes: Routes = [
   { path: 'event', component: EventComponent },
   { path: 'group', component: GroupComponent },
+  { path: 'group/:id', component: GroupDetailComponent },
   { path: 'groups', component: GroupsComponent }
 ];
 
